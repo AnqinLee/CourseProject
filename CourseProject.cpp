50ef9058040f1d5149a1e0860d14da398d877cb6
安秦 李 <3509764541@qq.com>
2026-04-30 23:06:32 +0800

    目录栏增加两个按钮，对颜色设置进行实现，大小设置暂未实现，等待下一步进行

--- CourseProject.cpp
+++ CourseProject.cpp
@@ -129,7 +129,7 @@ public:
 			{
 				if (currentGrid[i][j])
 				{
-					setfillcolor(RED);
+					setfillcolor(gridColor);
 					solidrectangle(j * (cellSize + 1), i * (cellSize + 1), (j + 1) * (cellSize + 1) - 2, (i + 1) * (cellSize + 1) - 2);
 				}
 			}
@@ -288,6 +288,7 @@ private:
 	int StartButtonWidth = windowWidth / 2;
 	int StartButtonHeight = windowHeight / 16;
 
+	bool isInSetColor = false;
 
 public:
 	initWindow(int cellSize = DEFAULTCELLSIZE, int gridWidth = DEFAULTGRIDWIDTH, int gridHeight = DEFAULTGRIDHEIGHT) :
@@ -305,8 +306,9 @@ public:
 		closegraph();
 	}
 
-	void drawButton(int buttonx, int buttony, int buttonWidth, int buttonHeight, LPCTSTR text, int nHeight = 16, COLORREF buttoncolor = RED, COLORREF textcolor = WHITE)
+	void drawButton(int buttonx, int buttony, int buttonWidth, int buttonHeight, LPCTSTR text, int nHeight = 16, COLORREF buttoncolor = RED)
 	{
+		COLORREF textcolor = RGB(255 - GetRValue(buttoncolor), 255 - GetGValue(buttoncolor), 255 - GetBValue(buttoncolor));
 		setfillcolor(buttoncolor);
 		solidrectangle(buttonx, buttony, buttonx + buttonWidth, buttony + buttonHeight);
 		settextcolor(textcolor);
@@ -315,7 +317,6 @@ public:
 		int spaceHeight = (buttonHeight - textheight(text)) / 2;
 		setbkmode(TRANSPARENT);
 		outtextxy(buttonx + spaceWidth, buttony + spaceHeight, text);
-		FlushBatchDraw();
 	}
 
 	void drawBackground(void)
@@ -333,9 +334,13 @@ public:
 
 	void drawMenu(void)
 	{
+		cleardevice();
 		drawBackground();
 		drawButton(windowWidth / 4, windowHeight / 16, windowWidth / 2, windowHeight / 16, _T("Game of Life"), 32, BLACK);
 		drawButton(StartButtonx, StartButtony, StartButtonWidth, StartButtonHeight, _T("Start"));
+		drawButton(StartButtonx, StartButtony * 2, StartButtonWidth, StartButtonHeight, _T("Set Color"));
+		drawButton(StartButtonx, StartButtony * 3, StartButtonWidth, StartButtonHeight, _T("Set Size"));
+		FlushBatchDraw();
 	}
 
 	bool isHangingOnStart(ExMessage& msg)
@@ -345,6 +350,48 @@ public:
 		return false;
 	}
 
+	bool isHangingOnSetColor(ExMessage& msg)
+	{
+		if (msg.x >= StartButtonx && msg.x <= StartButtonx + StartButtonWidth && msg.y >= StartButtony * 2 && msg.y <= StartButtonHeight + StartButtony * 2)
+			return true;
+		return false;
+	}
+
+	bool isHangingOnReturn(ExMessage& msg)
+	{
+		if (msg.x <= windowWidth / 8 && msg.y <= windowHeight / 16)
+			return true;
+		return false;
+	}
+
+	void drawSetColor()
+	{
+		cleardevice();
+		drawBackground();
+		drawButton(0, 0, windowWidth / 8, windowHeight / 16, _T("Return"));
+
+		TCHAR RValue[100];
+		_stprintf_s(RValue, _T("R Value=%d"), GetRValue(gridColor));
+		drawButton(StartButtonx, windowHeight * 7 / 32, StartButtonWidth, StartButtonHeight, RValue);
+
+		TCHAR GValue[100];
+		_stprintf_s(GValue, _T("G Value=%d"), GetGValue(gridColor));
+		drawButton(StartButtonx, windowHeight * 15 / 32, StartButtonWidth, StartButtonHeight, GValue);
+
+		TCHAR BValue[100];
+		_stprintf_s(BValue, _T("B Value=%d"), GetBValue(gridColor));
+		drawButton(StartButtonx, windowHeight * 23 / 32, StartButtonWidth, StartButtonHeight, BValue);
+
+		FlushBatchDraw();
+	}
+
+	void runGame(void)
+	{
+		EndBatchDraw();
+		closegraph();
+		GameOfLife game(cellSize, gridWidth, gridHeight, gridColor);
+		game.run();
+	}
 
 	void handleMessage(void)
 	{
@@ -357,8 +404,61 @@ public:
 				runGame();
 			break;
 		case WM_LBUTTONDOWN:
-			if (isHangingOnStart(msg))
-				runGame();
+			if (!isInSetColor)
+			{
+				if (isHangingOnStart(msg))
+					runGame();
+				if (isHangingOnSetColor(msg))
+				{
+					isInSetColor = true;
+					drawSetColor();
+				}
+			}
+			else
+			{
+				if (isHangingOnReturn(msg))
+				{
+					isInSetColor = false;
+					drawMenu();
+				}
+				else
+				{
+					if (msg.y <= windowHeight * 3 / 8)
+					{
+						if (msg.x <= windowWidth / 4)
+							gridColor = RGB((GetRValue(gridColor) + 245) % 255, GetGValue(gridColor), GetBValue(gridColor));
+						else if(msg.x<=windowWidth/2)
+							gridColor = RGB((GetRValue(gridColor) + 250) % 255, GetGValue(gridColor), GetBValue(gridColor));
+						else if(msg.x<=windowWidth*3/4)
+							gridColor = RGB((GetRValue(gridColor) + 5) % 255, GetGValue(gridColor), GetBValue(gridColor));
+						else
+							gridColor = RGB((GetRValue(gridColor) + 10) % 255, GetGValue(gridColor), GetBValue(gridColor));
+					}
+					else if (msg.y <= windowHeight * 5 / 8)
+					{
+						if (msg.x <= windowWidth / 4)
+							gridColor = RGB(GetRValue(gridColor), (GetGValue(gridColor) + 245) % 255, GetBValue(gridColor));
+						else if (msg.x <= windowWidth / 2)
+							gridColor = RGB(GetRValue(gridColor), (GetGValue(gridColor) + 250) % 255, GetBValue(gridColor));
+						else if (msg.x <= windowWidth * 3 / 4)
+							gridColor = RGB(GetRValue(gridColor), (GetGValue(gridColor) + 5) % 255, GetBValue(gridColor));
+						else
+							gridColor = RGB(GetRValue(gridColor), (GetGValue(gridColor) + 10) % 255, GetBValue(gridColor));
+					}
+					else
+					{
+						if (msg.x <= windowWidth / 4)
+							gridColor = RGB(GetRValue(gridColor), GetGValue(gridColor), (GetBValue(gridColor) + 245) % 255);
+						else if (msg.x <= windowWidth / 2)
+							gridColor = RGB(GetRValue(gridColor), GetGValue(gridColor), (GetBValue(gridColor) + 250) % 255);
+						else if (msg.x <= windowWidth * 3 / 4)
+							gridColor = RGB(GetRValue(gridColor), GetGValue(gridColor), (GetBValue(gridColor) + 5) % 255);
+						else
+							gridColor = RGB(GetRValue(gridColor), GetGValue(gridColor), (GetBValue(gridColor) + 10) % 255);
+					}
+					drawSetColor();
+				}
+			}
 			break;
 		}
 	}
@@ -372,13 +472,6 @@ public:
 		}
 	}
 
-	void runGame(void)
-	{
-		EndBatchDraw();
-		closegraph();
-		GameOfLife game(cellSize, gridWidth, gridHeight,gridColor);
-		game.run();
-	}
 };
 
 int main(void)
